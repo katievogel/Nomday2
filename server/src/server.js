442ff3ddf64e5d759e6773a464a455365a529f76
Katie Vogel <kvog983@gmail.com>
2020-03-28 21:04:45 -0400

    simplest express added

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,15 @@
+//Simplest express 
 var express = require('express');
 
-var PORT = process.env.PORT || 3000;
+var PORT = 3000;
 
 var app = express();
+app.get('/', function (req, res) {
+    res.send('hello world');
+});
 
 console.log('starting server')
 app.listen(PORT, function () {
     console.log(`server started on ${PORT}`);
 });
-
+//end simplest express
