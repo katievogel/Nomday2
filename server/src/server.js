8d3da875e951696b02f8015ae7d808cd272aea3e
Katie Vogel <kvog983@gmail.com>
2020-03-28 20:52:18 -0400

    express server start

--- server/src/server.js
+++ server/src/server.js
@@ -1,1 +1,11 @@
-console.log('hello world');
+var express = require('express');
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+console.log('starting server')
+app.listen(PORT, function () {
+    console.log(`server started on ${PORT}`);
+});
+
