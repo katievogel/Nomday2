742a76e3c55e16f8ec3de07f7095ae26dcefa0c0
Katie Vogel <kvog983@gmail.com>
2020-03-28 21:34:49 -0400

    get requests for users

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,28 @@
 //Simplest express 
 var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser');
 
 var PORT = 3000;
 
 var app = express();
-app.get('/', function (req, res) {
+app.use(cors());
+app.use(bodyParser.json({ limit: '50mb', parameterLimit: 100000 }));
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+app.post('/adduser', (req, res) => {
+    console.log('adduser received post');
+    console.log(`adduser request.body: ${JSON.stringify(req.body)}`);
+    // Add record to database
+    // username, email address, password
+    // unique user-id
+    res.send('adduser');
+});
+
+app.post('/get-all-users', (req, res) => {
     res.send('hello world');
 });
 
